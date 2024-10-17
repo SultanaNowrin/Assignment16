656fba26decbd063c6b1ae5699766f4ef841e891
SultanaNowrin <nowrin.sultana696@gmail.com>
2024-10-17 23:55:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/*function nowrin(){
+
+    document.body.backgroundcolour = 'red';
+
+}*/
+//document.getElementById('btn').addEventListener('click', document.body.style.backgroundColor = 'red')
+//console.log("nowrin")
+
+/*document.getElementById('id').addEventListener('click', function nowrin(){
+
+    document.body.style.backgroundColor = 'purple'
+            //console.log('nowrin') 
+})*/
+
+/*document.getElementById('btn').addEventListener('click')
+function nowrin(){
+        document.body.style.backgroundColor = 'blue'
+}*/
+
+const title = document.getElementById('btn')
+/*title.addEventListener('click', function nowrin(){
+       condsole.log('nowrin')
+})*/
+
+/*title.addEventListener('click', function(){
+    condsole.log('nowrin')
+})*/
+
+title.addEventListener('click', function(){
+    const para = getElementById('para')
+    para.innerText = "hi"
+    condsole.log(para)
+})
+
+
+
+
+
+
+
+
+
+
+
+
